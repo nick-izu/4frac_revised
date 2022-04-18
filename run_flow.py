6ade92525b8a1bcadeceab94ea0cf21d8570dc7f
nick-izu <51707671+nick-izu@users.noreply.github.com>
2022-04-18 14:46:37 +0200

    Update run_flow.py

--- run_flow.py
+++ run_flow.py
@@ -13,27 +13,55 @@ from pydfnworks import *
 import subprocess
 from numpy import *
 
-define_paths()
 main_time = time()
 DFN = create_dfn()
 
-##dfnGen
 DFN.make_working_directory()
 DFN.check_input()
 DFN.create_network()
-DFN.mesh_network(visual_mode=False)
+
+DFN.output_report()
+
+DFN.set_flow_solver("PFLOTRAN")
+DFN.mesh_network(uniform_mesh=True)
+
+DFN.lagrit2pflotran()
+
+restart_file = "DUMMY/dfnworks-main/examples/ade_example/dfn_restart.in"
+
+DFN.pflotran(transient=True,restart=True,restart_file=restart_file)
+DFN.parse_pflotran_vtk_python()       
+DFN.pflotran_cleanup()
+DFN.pflotran_cleanup(index_finish=100,filename=restart_file)
+
+
+
+
+
+
+
+
+#define_paths()
+#main_time = time()
+#DFN = create_dfn()
+
+##dfnGen
+#DFN.make_working_directory()
+#DFN.check_input()
+#DFN.create_network()
+#DFN.mesh_network(visual_mode=False)
 
 ##dfnFlow
 #DFN.pflotran()
 #DFN.parse.pflotran_vtk_python()
 #DFN.pflotran_cleanup()
 
-DFN.dfn_flow()
+#DFN.dfn_flow()
 
 ##dfnTrans
 #DFN.copy_dfn_trans_files()
 #DFN.check_dfn_trans_run_files()
 #DFN.run_dfn_trans()
 
-DFN.dfn_trans()
+#DFN.dfn_trans()
 
