e3c0dad4266d7fe78dbe4a4d8204ee3d77619ce9
nick-izu <51707671+nick-izu@users.noreply.github.com>
2022-05-24 21:41:05 +0200

    Update run_flow.py

--- run_flow.py
+++ run_flow.py
@@ -31,39 +31,43 @@ DFN.mesh_network(visual_mode=False)
 
 ## JDH This portion looks good. 
 # First we define values for the stochastic families
-porosity = 1.0
-tortuosity= 1.0
 
-variable = "transmissivity"
-function = "correlated"
-params = {"alpha":2.2*10**-9, "beta":0.8}
-b1,perm1,T1 = DFN.generate_hydraulic_values(variable,function,params,family_id=1)
+#variable = "transmissivity"
+#function = "correlated"
+#params = {"alpha":2.2*10**-9, "beta":0.8}
+#b1,perm1,T1 = DFN.generate_hydraulic_values(variable,function,params,family_id=1)
 
-function = "correlated"
-params = {"alpha":2.2*10**-9, "beta":0.8}
-b2,perm2,T2 = DFN.generate_hydraulic_values(variable,function,params,family_id=2)
+#function = "correlated"
+#params = {"alpha":2.2*10**-9, "beta":0.8}
+#b2,perm2,T2 = DFN.generate_hydraulic_values(variable,function,params,family_id=2)
 
-function = "correlated"
-params = {"alpha":2.2*10**-9, "beta":0.8}
-b3,perm3,T3 = DFN.generate_hydraulic_values(variable,function,params,family_id=3)
+#function = "correlated"
+#params = {"alpha":2.2*10**-9, "beta":0.8}
+#b3,perm3,T3 = DFN.generate_hydraulic_values(variable,function,params,family_id=3)
+
 # Then we combine them.
-T = T1 + T2 + T3 
-b = b1 + b2 + b3
-perm = perm1 + perm2 + perm3 
+#T = T1 + T2 + T3 
+#b = b1 + b2 + b3
+#perm = perm1 + perm2 + perm3 
+
 # Entires 0-3, should be zero here. 
-print(b[0:3])
+print(b[0:5])
 
 # JDH- now assigne each of those values for aperture
 b[0] = 1*10**-3 
 b[1] = 1*10**-3 
 b[2] = 1*10**-3 
-b[3] = 5*10**-4
+b[3] = 1*10**-3 
+b[4] = 5*10**-4
+b[5] = 5*10**-4
 
 # convert perm using the cubic law
 perm[0] = b[0]**2/12
 perm[1] = b[1]**2/12
 perm[2] = b[2]**2/12
 perm[3] = b[3]**2/12
+perm[4] = b[2]**2/12
+perm[5] = b[3]**2/12
 
 # determine values of Transmissivity for determinsitc fractures
 mu = 8.9e-4  #dynamic viscosity of water at 20 degrees C, Pa*s
@@ -74,6 +78,8 @@ T[0] = (b[0]**3 * rho * g) / (12 * mu)
 T[1] = (b[1]**3 * rho * g) / (12 * mu)
 T[2] = (b[2]**3 * rho * g) / (12 * mu)
 T[3] = (b[3]**3 * rho * g) / (12 * mu)
+T[4] = (b[2]**3 * rho * g) / (12 * mu)
+T[5] = (b[3]**3 * rho * g) / (12 * mu)
 
 DFN.dump_hydraulic_values(b,perm,T)
 
