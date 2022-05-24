be73e98969b5c723f91dee14b195a54c031fe6c5
nick-izu <51707671+nick-izu@users.noreply.github.com>
2022-05-24 22:25:57 +0200

    Update run_flow.py

--- run_flow.py
+++ run_flow.py
@@ -66,8 +66,8 @@ perm[0] = b[0]**2/12
 perm[1] = b[1]**2/12
 perm[2] = b[2]**2/12
 perm[3] = b[3]**2/12
-perm[4] = b[2]**2/12
-perm[5] = b[3]**2/12
+perm[4] = b[4]**2/12
+perm[5] = b[5]**2/12
 
 # determine values of Transmissivity for determinsitc fractures
 mu = 8.9e-4  #dynamic viscosity of water at 20 degrees C, Pa*s
@@ -78,8 +78,8 @@ T[0] = (b[0]**3 * rho * g) / (12 * mu)
 T[1] = (b[1]**3 * rho * g) / (12 * mu)
 T[2] = (b[2]**3 * rho * g) / (12 * mu)
 T[3] = (b[3]**3 * rho * g) / (12 * mu)
-T[4] = (b[2]**3 * rho * g) / (12 * mu)
-T[5] = (b[3]**3 * rho * g) / (12 * mu)
+T[4] = (b[4]**3 * rho * g) / (12 * mu)
+T[5] = (b[5]**3 * rho * g) / (12 * mu)
 
 DFN.dump_hydraulic_values(b,perm,T)
 
