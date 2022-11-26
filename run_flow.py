2ccd9a189f9f134e5c0b1735fd23085dd31e48db
nick-izu <51707671+nick-izu@users.noreply.github.com>
2022-11-26 23:06:39 +0100

    Update run_flow.py

--- run_flow.py
+++ run_flow.py
@@ -1,18 +1,42 @@
-"""
-Created on Thu Sep 10 12:04:03 2020
-@author: rosie
-"""
-
-import os, sys
-from time import time
-from pydfnworks import * 
-from numpy import *
+from pydfnworks import *
 
 DFN = create_dfn()
-# General Work Flow
-DFN.dfn_gen()
+
+##dfnGen
+DFN.make_working_directory()
+DFN.check_input()
+DFN.create_network()
+DFN.mesh_network(visual_mode=False)
+
+# First we define values for the stochastic families
+
+variable = "transmissivity"
+function = "correlated"
+params = {"alpha":2.2*10**-9, "beta":0.7}
+b1,perm1,T1 = DFN.generate_hydraulic_values(variable,function,params,family_id=1)
+b2,perm2,T2 = DFN.generate_hydraulic_values(variable,function,params,family_id=1)
+b3,perm3,T3 = DFN.generate_hydraulic_values(variable,function,params,family_id=1)
+b4,perm4,T4 = DFN.generate_hydraulic_values(variable,function,params,family_id=1)
+b5,perm5,T5 = DFN.generate_hydraulic_values(variable,function,params,family_id=1)
+b6,perm6,T6 = DFN.generate_hydraulic_values(variable,function,params,family_id=1)
+
+DFN.dump_hydraulic_values(b,perm,T)
+
+# Add transmissivity values to the mesh for visualization
+DFN.add_variable_to_mesh("trans,", "transmissivity.dat", "full_mesh.inp")
+
+## JDH I added an exit here for you to look at the files before you run the simulation.
+#exit()
+
+#dfnFlow
 DFN.dfn_flow()
+
+
+#dfntrans
 DFN.dfn_trans()
+Footer
+© 2022 GitHub, Inc.
+
 
 
 # Entires 0-3, should be zero here. 
@@ -33,6 +57,8 @@ DFN.dfn_trans()
 #perm[3] = b[3]**2/12
 #perm[4] = b[4]**2/12
 #perm[5] = b[5]**2/12
+
+
 
 # determine values of Transmissivity for determinsitc fractures
 #mu = 8.9e-4  #dynamic viscosity of water at 20 degrees C, Pa*s
