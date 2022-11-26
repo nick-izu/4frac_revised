d0a9cd74954c5d6a3738c78faed6102b54af2f72
nick-izu <51707671+nick-izu@users.noreply.github.com>
2022-11-26 23:21:04 +0100

    Update run_flow.py

--- run_flow.py
+++ run_flow.py
@@ -8,25 +8,6 @@ DFN.check_input()
 DFN.create_network()
 DFN.mesh_network(visual_mode=False)
 
-# First we define values for the stochastic families
-
-variable = "transmissivity"
-function = "correlated"
-params = {"alpha":2.2*10**-9, "beta":0.7}
-b1,perm1,T1 = DFN.generate_hydraulic_values(variable,function,params,family_id=1)
-b2,perm2,T2 = DFN.generate_hydraulic_values(variable,function,params,family_id=1)
-b3,perm3,T3 = DFN.generate_hydraulic_values(variable,function,params,family_id=1)
-b4,perm4,T4 = DFN.generate_hydraulic_values(variable,function,params,family_id=1)
-b5,perm5,T5 = DFN.generate_hydraulic_values(variable,function,params,family_id=1)
-b6,perm6,T6 = DFN.generate_hydraulic_values(variable,function,params,family_id=1)
-
-DFN.dump_hydraulic_values(b,perm,T)
-
-# Add transmissivity values to the mesh for visualization
-DFN.add_variable_to_mesh("trans,", "transmissivity.dat", "full_mesh.inp")
-
-## JDH I added an exit here for you to look at the files before you run the simulation.
-#exit()
 
 #dfnFlow
 DFN.dfn_flow()
@@ -34,8 +15,7 @@ DFN.dfn_flow()
 
 #dfntrans
 DFN.dfn_trans()
-Footer
-© 2022 GitHub, Inc.
+
 
 
 
